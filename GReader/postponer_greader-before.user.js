96f12c267c88197d09e832d6fccc31dc599ce47f
Juliana Pena <limited.mage@gmail.com>
2010-02-04 22:37:48 -0600

    Version 0.3 - 4 Feb 09 Manager  * Now badge only reloads when Chrome is opened and when the icon is clicked. Previous version was running out of API calls within seconds and overloading the RIL server. Sorry! Just remember to click the icon to reload. Adder  * Google Reader integration! Now an icon appears next to the title of articles in Google Reader. When clicked, a window appears to add the item to RIL. Clicking this icon also reloads the badge in the Manager.  * Options page to turn Google Reader integration on/off, change the position of the icon to before or after the title of the article, and whether to show the icon in list view, expanded view, or both.

---
+++ GReader/postponer_greader-before.user.js
@@ -0,0 +1,102 @@
+// modified from original by Hiroshi Miyazaki
+// see http://forgotthemilk.net/labo/rememberthemilk-cow-in-googlereader
+
+(function() {
+
+    var timerID;
+    var busy = false;
+
+    //postponer adder small icon image(base64)
+    var icon = 'data:image/png;base64,'+
+    'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABHNCSVQICAgIfAhkiAAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAAKbSURBVDiNjZLLa1RXHMc/v3PPzZlM5uYyPqJjGh8J2El14wNUAi5MrZBdQ7GiLgRFcWX/B6ELd92XCrpSEMWFj4UPfC7ETSio2CJIG20ezuNOxsy9d85xcSdj7Mrv5seB8338fnzFOcehwwe3rls3+Gv52vXtJo576EAZEbXGJPWJbW8Hvwu9neVbf4ZD4TWgKmJf5PMv5jTA8Kbh344dO77vTdKmNDdPyffBAxXELA5ZooGFUvDtW3Iq2BlNR7uDUuESzhtq1bY+1ACiRPtas2L8eyrnzpE8foy3UtOz2VAYHqR/OCFvAsJNU1T+2liOq7Wjus/cx8+L7sZVCgFckoCAGAh+FgrjYFYdwRRPIqLpX3+Xxer5kXT+4qDN1YuKZRCRbPqCF3rY9xrxhNzKX/B6hgDQZjO9xdM419a6UKx3BCQjO5c9LeAJ7qNHOt+k+e7Mch8a0yegv1AT37cqo2fuIoJSil6Tx9gcOunBNT1QOZxtkSzcw7kUUT7OuYZYqXdvIJIJad+nEIaIX8CPYlzVJ4luU1vcgjLfYN/P4NJZWi58Z/rsq0zAfY6ntSYol/k7jllPH+51g6i3RXHvDpREKLeBD7Mf2o3o3+fh6uZVxdIOCALYdpupapX22BhPH7ykMdVPfWoA2IWoMZA9zNXGk9//KF3J51/+p7sBJLvDmydPGD17lg2nTrFmcpLnExOsVT+gvB9xpIgy1GrP0n+mH30E0CzDitFRtl24wMCBA4gIxXKZPXfuIGmKUgaRHM655ZQvBUwQ0Lt//xcfgpERPM/LiiaCtbazcqeAfAWWXP/v3k0Qt+La7MyMs9bKUh8QQXWmiHRNHVCpVARoAohzjoOHfiqmaTqptW5/RRppRI2FmzduXwb4BKdI8d5aEhrxAAAAAElFTkSuQmCC';
+
+    //make RIL icon as earch article
+    function setRilIcon(targetNode, href, titleStr) {
+        //make linking string
+        var anker = "javascript:(\
+				function(){\
+					name=encodeURIComponent(\""+titleStr+"\");\
+					url=\""+href+"\";\
+					cp=\"https://readitlaterlist.com/edit?BL=1&title=\"+name+\"&url=\"+url;\
+					w=window.open(cp,\"_blank\",\"status=no,toolbar=no,width=320,height=220,resizable=yes\");\
+					setTimeout(function(){w.focus();},500);\
+				})();";
+        //make anker tags
+        var a = document.createElement("a");
+        a.setAttribute('href', anker);
+        //make img tag
+        var imgTag = document.createElement("img");
+        imgTag.src = icon;
+        imgTag.border="0px";
+        //set img tag in anker tag
+        a.appendChild(imgTag);
+        //set anker tag in span tag
+        var before = targetNode.firstChild;
+        with (targetNode) {
+            appendChild(document.createTextNode(" "));
+            appendChild(a);
+        }
+    }
+
+    //make RIL icons
+    function setRilIcons(titleArray) {
+        //loop for articles
+        for (var i = 0; i < titleArray.length; i++) {
+            var href = titleArray[i].href;
+            var title = titleArray[i].node;
+            var titleStr = titleArray[i].titleStr;
+            //make span tag
+            var node = document.createElement('span');
+            node.className = 'googlereader2ril';
+            //make RIL icon
+            setRilIcon(node, href, titleStr);
+            title.insertBefore(node, title.childNodes[0]);
+        }
+    }
+
+    //main function(timer calls every 3 seconds)
+    function greader_add_rilicon() {
+        if (busy) return;
+
+        var titles = document.evaluate('//h2[@class="entry-title"]', document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
+        if (! titles.snapshotLength) return;
+
+        busy = true;
+
+        var titleArray = new Array();
+        for (var i = 0; i < titles.snapshotLength; i++) {
+            var title = titles.snapshotItem(i);
+            var nodes = title.childNodes;
+            if ((nodes == null) ||
+              (nodes[0] == null) ||
+              (nodes[0].tagName != 'SPAN') ||
+              (nodes[0].className != 'googlereader2ril')) {
+                var link = null;
+                var titleStr = '';
+                if (title.firstChild.tagName == 'A') {
+                    // entry-container (Expanded view or Collapsed item)
+                    link = title.firstChild;
+                    titleStr = link.firstChild.textContent;
+                } else {
+                    // entry (List view)
+                    link = title.parentNode.parentNode.firstChild;
+                    if (link.tagName != 'A') link = null;
+                    titleStr = title.textContent;
+                }
+                if (link != null) {
+                    titleArray.push({ node: title, href: link.href, titleStr: titleStr });
+                }
+            }
+        }
+        if (titleArray.length == 0) {
+            busy = false;
+            return;
+        }
+        //make RIL icons
+        setRilIcons(titleArray);
+        busy = false;
+    }
+
+    // be careful not to be too busy
+    timerID = setInterval(greader_add_rilicon, 3000);
+})();
--- README.txt
+++ README.txt
@@ -2,20 +2,27 @@ Postponer is a pair of Chrome extensions to add to and manage your Read It Later
 
 Postponer integrates RIL's great bookmarklets (available at http://readitlaterlist.com/bookmarklets) with Chrome's interface for a simple way to manage your reading list.
 
-Postponer Adder (https://chrome.google.com/extensions/detail/pggmlienkcoenodbjpkbidlmmedgonai) adds an icon in the Chrome address bar for quickly adding items to your reading list. It can also show the number of unread icons in your list on an icon badge (go to the Options page to enable this).
+Postponer Adder (https://chrome.google.com/extensions/detail/pggmlienkcoenodbjpkbidlmmedgonai) adds an icon in the Chrome address bar for quickly adding items to your reading list. It also adds an icon next to every article Google Reader to add it to your reading list.
 
-Postponer Manager (https://chrome.google.com/extensions/detail/mmfblgljgoaokhbcjnddgcnaielcpjeb) adds an icon in the Chrome toolbar to open your reading list with all your unread items. From there, you can open items, mark as read, etc.
+Postponer Manager (https://chrome.google.com/extensions/detail/mmfblgljgoaokhbcjnddgcnaielcpjeb) adds an icon in the Chrome toolbar to open your reading list with all your unread items. From there, you can open items, mark as read, etc. It can also show the number of unread items in your list on an icon badge (go to the Options page to enable this).
 
 The extensions were designed to be used together, but can also be used on their own. The reason why there are two extensions is because Chrome does not allow one extension to add icons to both the address bar and the toolbar.
 
 Changelog
+
+Version 0.3 - 4 Feb 09
+Manager
+ * Now badge only reloads when Chrome is opened and when the icon is clicked. Previous version was running out of API calls within seconds and overloading the RIL server. Sorry! Just remember to click the icon to reload.
+Adder
+ * Google Reader integration! Now an icon appears next to the title of articles in Google Reader. When clicked, a window appears to add the item to RIL. Clicking this icon also reloads the badge in the Manager.
+ * Options page to turn Google Reader integration on/off, change the position of the icon to before or after the title of the article, and whether to show the icon in list view, expanded view, or both.
 
 Version 0.2 - 23 Dec 09
 Manager
  * Added title to browser action icon
  * Icon can now show number of unread items in a badge
  * Added an options page for enabling badge and checking credentials
- * Added sheduling for badge reload when icon in clicked and external reload request (ie, from Postponer Adder) is sent
+ * Added scheduling for badge reload when icon in clicked and external reload request (ie, from Postponer Adder) is sent
 Adder
  * Added communication with Manager so badge is scheduled to reload when Adder icon is clicked.
 
